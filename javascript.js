d44e74c5ec227e36cc9ba77355856253f7ddb225
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-05-30 20:27:46 +0200

    JavaScript_Projekt

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const zahl = +(prompt("Geben Sie bitte eine Zahl ein!"));
+
+a = "GeradeZahl"
+
+const geradeZahl = zahl%2==0 ? a = "GeradeZahl" : a = "ungeradeZahl";
+
+console.log("eingegebene Zahl :", `${zahl}`, a)
